fbb7469e1aca9b3fe10cdf69dd0e72c3030d4639
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-05 11:15:25 -0500

    Update lfa_process.ps1

--- lfa_process.ps1
+++ lfa_process.ps1
@@ -2,7 +2,7 @@ $targets = Import-Csv .\WinHost.csv |
     Where-Object {$_.os -eq "Win10"} |
         Select-Object -ExpandProperty ip
 
-$current = Invoke-Command -ComputerName $targets -Credential $creds -FilePath ..\3317\Scripts\procs.ps1
+$current = Invoke-Command -ComputerName $targets -Credential $creds -FilePath ..\3317\Scripts\processes.ps1
 
 $current | Sort-Object -Property pscomputername, hash -Unique |
     Group-Object hash |

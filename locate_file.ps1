b556715bd6bbee1fd6be13f5817e113e434842b8
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-05 14:16:24 -0500

    Delete locate_file.ps1

--- locate_file.ps1
+++
@@ -1,4 +0,0 @@
-Invoke-Command -ComputerName 192.168.0.1 -Credential $creds -ScriptBlock {
-    $Paths = "C:Windows\System32\Hidden", "C:\Program Files\Downloads"
-    Get-ChildItem -Path $Paths -Recurse -File -Force
-} 

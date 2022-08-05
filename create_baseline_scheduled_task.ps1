98f08efbb8778acaf718099bc6113ddabab32eee
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-05 14:15:35 -0500

    Delete create_baseline_scheduled_task.ps1

--- create_baseline_scheduled_task.ps1
+++
@@ -1,5 +0,0 @@
-$targets = Import-Csv .\Winhosts.csv |
-  Where-Object {$_.os -eq "Win10"} |
-    Select-Object -ExpandProperty ip
-
-ICM -CN $targets -CR $creds -FilePath .\scheduled_task.ps1 | Export-Csv .\Win10ScheduledTaskBaseline.csv

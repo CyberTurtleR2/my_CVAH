93b3ca6918e1f4dc0ca4e072d06f26eff34a6f63
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-05 14:15:23 -0500

    Delete create_baseline_firewalls.ps1

--- create_baseline_firewalls.ps1
+++
@@ -1,5 +0,0 @@
-$targets = Import-Csv .\Winhosts.csv |
-  Where-Object {$_.os -eq "Win10"} |
-    Select-Object -ExpandProperty ip
-
-ICM -CN $targets -CR $creds -FilePath .\firewall.ps1 | Export-Csv .\Win10FirewallBaseline.csv

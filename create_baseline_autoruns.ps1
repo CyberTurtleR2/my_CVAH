43ad65362d357f123a3e82d888eb4fdeea40a3e9
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-05 14:15:17 -0500

    Delete create_baseline_autoruns.ps1

--- create_baseline_autoruns.ps1
+++
@@ -1,5 +0,0 @@
-$targets = Import-Csv .\Winhosts.csv |
-  Where-Object {$_.os -eq "Win10"} |
-    Select-Object -ExpandProperty ip
-
-ICM -CN $targets -CR $creds -FilePath .\autoruns.ps1 -ArgumentList (,(Get-Content .\Autoruns.txt)) | Export-Csv .\Win10AutoRunsBaseline.csv

228e19c587037f5e46acc67e53154ff58cbdfd79
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-05 14:21:29 -0500

    Update copy_session.ps1

--- 05_Misc_Scripts/copy_session.ps1
+++ 05_Misc_Scripts/copy_session.ps1
@@ -16,3 +16,10 @@ $session = New-PSSession -ComputerName 192.168.0.1 -Credential $creds
 Copy-Item -Path C:\Users\student\Documents\file.name.txt -FromSession $session -Destination .
 
 Remove-PSSession *
+
+
+### Taking a Hash ###
+
+Get-FileHash -Algorithm SHA1 .\file.name.txt
+Get-FileHash -Algorithm SHA256 .\file.name.txt
+Get-FileHash -Algorithm MD5 .\file.name.txt

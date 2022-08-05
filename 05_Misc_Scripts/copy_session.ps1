b55e8737dcfe9945211f770f88909894782f5f69
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-05 14:22:30 -0500

    Update copy_session.ps1

--- 05_Misc_Scripts/copy_session.ps1
+++ 05_Misc_Scripts/copy_session.ps1
@@ -20,6 +20,6 @@ Remove-PSSession *
 
 ### Taking a Hash ###
 
-Get-FileHash -Algorithm SHA1 .\file.name.txt
-Get-FileHash -Algorithm SHA256 .\file.name.txt
-Get-FileHash -Algorithm MD5 .\file.name.txt
+Get-FileHash .\file.name.txt -Algorithm SHA1
+Get-FileHash .\file.name.txt -Algorithm SHA256
+Get-FileHash .\file.name.txt -Algorithm MD5

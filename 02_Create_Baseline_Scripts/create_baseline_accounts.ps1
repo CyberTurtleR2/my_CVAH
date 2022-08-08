ea720d7458c6fbef034c4ef817917afe6c0c7ea7
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-08 08:31:53 -0500

    Update create_baseline_accounts.ps1

--- 02_Create_Baseline_Scripts/create_baseline_accounts.ps1
+++ 02_Create_Baseline_Scripts/create_baseline_accounts.ps1
@@ -2,4 +2,4 @@ $targets = Import-Csv .\Winhosts.csv |
   Where-Object {$_.os -eq "Win10"} |
     Select-Object -ExpandProperty ip
 
-ICM -CN $targets -CR $creds -FilePath .\accounts.ps1 | Export-Csv .\Win10AccountsBaseline.csv
+ICM -CN $targets -CR $creds -FilePath ..\01_Reference_Scripts\accounts.ps1 | Export-Csv .\Win10AccountsBaseline.csv

a2a495bf4aa95147c7e547f703d58e8d8b69e716
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-08 09:08:36 -0500

    Update compare_baseline_autoruns.ps1

--- 03_Compare_Baseline_Scripts/compare_baseline_autoruns.ps1
+++ 03_Compare_Baseline_Scripts/compare_baseline_autoruns.ps1
@@ -1,14 +1,14 @@
-$targets = Import-Csv .\Winhosts.csv |
+$targets = Import-Csv ..\AllHosts.csv |
   Where-Object {$_.os -like "*2012*"} |
     Select-Object -ExpandProperty ip
     
 $ht = @{
-  ReferenceObject = Import-Csv .\Rubrics\WinServer2012AutoBaseline.csv
+  ReferenceObject = Import-Csv ..\02_Create_Baseline_Scripts\Win10AutoRunsBaseline.csv
   Property        = "Key_ValueName"
   PassThru        = $true
 }
 
-$current = Invoke-Command -ComputerName $targets -Credential $creds -FilePath ..\3317\Scripts\autoruns.ps1 -ArgumentList (,(Get-Content .\Autorunkeys.txt))
+$current = Invoke-Command -ComputerName $targets -Credential $creds -FilePath ..\01_Reference_Scripts\autoruns.ps1 -ArgumentList (,(Get-Content ..\Autorunkeys.txt))
 
 ForEach ($ip in $targets){
   $ht.DifferenceObject = $current |

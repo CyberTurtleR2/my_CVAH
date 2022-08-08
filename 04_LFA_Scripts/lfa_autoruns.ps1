4db75fee551ce85b94859272f102aad673f0f0fa
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-08 09:17:38 -0500

    Update lfa_autoruns.ps1

--- 04_LFA_Scripts/lfa_autoruns.ps1
+++ 04_LFA_Scripts/lfa_autoruns.ps1
@@ -1,8 +1,8 @@
-$targets = Import-Csv .\WinHost.csv |
+$targets = Import-Csv ..\AllHost.csv |
     Where-Object {$_.os -eq "Win10"} |
         Select-Object -ExpandProperty ip
 
-$current = Invoke-Command -ComputerName $targets -Credential $creds -FilePath ..\3317\Scripts\autoruns.ps1 -ArgumentList (Get-Content ..\3317\AutoRunKeys.txt)
+$current = Invoke-Command -ComputerName $targets -Credential $creds -FilePath ..\01_Reference_Scripts\autoruns.ps1 -ArgumentList (Get-Content ..\AutoRunKeys.txt)
 
 $current | Sort-Object -Property pscomputername, Key_ValueName -Unique |
     Group-Object Key_ValueName |

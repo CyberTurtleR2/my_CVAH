c5f65818ece326f9935fccc90ae154249c332653
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-08 09:16:00 -0500

    Update and rename compare_baseline_scheduledtask.ps1 to compare_baseline_scheduledtasks.ps1

--- 03_Compare_Baseline_Scripts/compare_baseline_scheduledtasks.ps1
+++ 03_Compare_Baseline_Scripts/compare_baseline_scheduledtasks.ps1
@@ -1,14 +1,14 @@
-$targets = Import-Csv .\Winhosts.csv |
+$targets = Import-Csv .\AllHosts.csv |
   Where-Object {$_.os -eq "Win10"} |
     Select-Object -ExpandProperty ip
     
 $ht = @{
-  ReferenceObject = Import-Csv .\Rubrics\Win10ScheduledTaskBaseline.csv
+  ReferenceObject = Import-Csv ..\02_Create_Baseline_Scripts\Win10ScheduledTasksBaseline.csv
   Property        = "taskname"
   PassThru        = $true
 }
 
-$current = Invoke-Command -ComputerName $targets -Credential $creds -FilePath ..\3317\Scripts\ScheduledTask.ps1 
+$current = Invoke-Command -ComputerName $targets -Credential $creds -FilePath ..\01_Reference_Scripts\ScheduledTask.ps1 
 
 ForEach ($ip in $targets){
   $ht.DifferenceObject = $current |

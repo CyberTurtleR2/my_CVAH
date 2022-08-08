8df6b8052b2a7ced5d6d5a18cd37b93c5a50f139
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-08 09:16:53 -0500

    Update compare_baseline_services.ps1

--- 03_Compare_Baseline_Scripts/compare_baseline_services.ps1
+++ 03_Compare_Baseline_Scripts/compare_baseline_services.ps1
@@ -1,14 +1,14 @@
-$targets = Import-Csv .\Winhosts.csv |
+$targets = Import-Csv .\AllHosts.csv |
   Where-Object {$_.os -eq "Win10"} |
     Select-Object -ExpandProperty ip
     
 $ht = @{
-  ReferenceObject = Import-Csv .\Rubrics\Win7ServiceBaseline.csv
+  ReferenceObject = Import-Csv ..\02_Create_Baseline_Scripts\Win10ServicesBaseline.csv
   Property        = "ServiceName"
   PassThru        = $true
 }
 
-$current = Invoke-Command -ComputerName $targets -Credential $creds -FilePath ..\3317\Scripts\services.ps1
+$current = Invoke-Command -ComputerName $targets -Credential $creds -FilePath ..\01_Reference_Scripts\services.ps1
 
 ForEach ($ip in $targets){
   $ht.DifferenceObject = $current |

5247bcfa3bbd92cc7ff9d05889d72a4064cf7e35
Michel Hallit <110614506+mhallit13@users.noreply.github.com>
2022-08-08 08:32:20 -0500

    Update create_baseline_autoruns.ps1

--- 02_Create_Baseline_Scripts/create_baseline_autoruns.ps1
+++ 02_Create_Baseline_Scripts/create_baseline_autoruns.ps1
@@ -2,4 +2,4 @@ $targets = Import-Csv .\Winhosts.csv |
   Where-Object {$_.os -eq "Win10"} |
     Select-Object -ExpandProperty ip
 
-ICM -CN $targets -CR $creds -FilePath .\autoruns.ps1 -ArgumentList (,(Get-Content .\Autoruns.txt)) | Export-Csv .\Win10AutoRunsBaseline.csv
+ICM -CN $targets -CR $creds -FilePath ..\01_Reference_Scripts\autoruns.ps1 -ArgumentList (,(Get-Content .\Autoruns.txt)) | Export-Csv .\Win10AutoRunsBaseline.csv
